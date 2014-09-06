77bfc2483646ce964e6d18c6e3f36766f9d73ee8
Jay Pinho <jaypinho@gmail.com>
2014-09-06 14:55:55 -0400

    Output results to CSV

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+
+	require 'net/http'
+	require 'csv'
+
+	def find_http_response(filename)
+
+		url = Array.new
+		csv_output = Array.new
+
+		CSV.foreach(filename, :headers => false) do |row|
+  			url << row[0].to_s
+		end
+
+		url.each do |x|
+
+			uri = URI(x)
+			res = Net::HTTP.get_response(uri)
+			csv_output << [x, res.code, res.message]
+
+			sleep 2
+
+		end
+
+		CSV.open("/Users/jaypinho/Desktop/fileoutput.csv", "wb") do |csv|
+			csv_output.each do |y|
+				csv << y
+			end
+		end
+
+	end
+
 end
