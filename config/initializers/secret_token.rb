fbc94902cb3309413286d6a522665fc285afcb29
Jay Pinho <jaypinho@gmail.com>
2014-09-05 23:09:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkfixer::Application.config.secret_key_base = 'cbdb83ef77bb0e70cee05aa8d605c6aa368e67f966f162d11219a26dae9961a5359c91fdd9857d357099d0d4ef5525d99907bbadf0cf42a7b13c0efc815aa32f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkfixer::Application.config.session_store :cookie_store, key: '_linkfixer_session'
