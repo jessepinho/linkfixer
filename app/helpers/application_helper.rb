4d532de9c24557ed2920cd5e035f8a6be8a9ebb4
Jay Pinho <jaypinho@gmail.com>
2014-09-11 17:11:35 -0400

    Add comments for clarity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,23 @@ module ApplicationHelper
 	require 'net/http'
 	require 'csv'
 
+	# This method gets the HTTP response information
 	def find_http_response(filename)
 
 		url = Array.new
 		csv_output = Array.new
 
+		# Pass the contents of the CSV file into an array
 		CSV.foreach(filename, :headers => false) do |row|
   			url << row
+  			# Enforce row limit of 50
   			break if url.length == 50
 		end
 
+		# Parse through each row
 		url.each do |x|
 
+			# Select the last (right-most) field of each row
 			last_col = x[x.length - 1].to_s
 
 			begin
@@ -24,15 +29,17 @@ module ApplicationHelper
 
 				case res
 				when Net::HTTPRedirection
+					# If redirected, find the final destination URL
 					final_redir = get_final_redirect(res['location'])
 					csv_output << [*x, res.code, res.message, final_redir]
 				else
 					csv_output << [*x, res.code, res.message, ""]
 				end
 				
-				# puts last_col
+				# Un-comment the below "sleep" method in order to enable a 1-second pause between pings
 				# sleep 1
 
+			# In case of error...
 			rescue
 
 				csv_output << [*x, "", "Couldn't ping", ""]
@@ -42,6 +49,7 @@ module ApplicationHelper
 
 		end
 
+		# Create the final CSV output file
 		csv_outcome = CSV.generate("") do |csv|
 			csv_output.each do |cf|
 				csv << cf
@@ -50,37 +58,9 @@ module ApplicationHelper
 
 		return csv_outcome
 
-=begin
-		output_file = File.dirname(filename).to_s
-		unless (output_file.last == "/") or (output_file.last == "\\")
-			if output_file.include? "\\"
-				output_file += "\\"
-			else
-				output_file += "/"
-			end
-		end
-		output_file += File.basename(filename).to_s.first(File.basename(filename).to_s.length - 4)
-		output_file += "_output.csv"
-
-		CSV.open(output_file, "wb") do |csv|
-			csv_output.each do |y|
-				csv << y
-			end
-		end
-=end
-
 	end
 
-	def testsomething
-		testvar = ["hello", "goodbye"]
-		testvar2 = [*testvar, "and adieu"]
-		puts testvar2.inspect
-
-		puts File.dirname("/Users/jaypinho/Desktop/fileoutput.csv")
-		puts File.basename("/Users/jaypinho/Desktop/fileoutput.csv")
-
-	end
-
+	# This method gets the final redirect of a given URL, up to a maximum of 5 successive redirects (which can be changed to any other number)
 	def get_final_redirect(page_url, limit = 5)
 
 		raise ArgumentError, 'Too many HTTP redirects' if limit == 0
@@ -91,6 +71,7 @@ module ApplicationHelper
 
 			case res
 			when Net::HTTPRedirection
+				# Recursively calls the same method until we hit a non-redirected URL or reach 5 redirects
 				get_final_redirect(res['location'], limit - 1)
 			else
 				page_url
